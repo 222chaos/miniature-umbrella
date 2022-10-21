f201f5140008fd8d148baa016c00755e5434af82
Your Name <you@example.com>
2022-10-21 11:17:51 +0800

    优化

--- chunk.js
+++ chunk.js
@@ -51,6 +51,24 @@ function chunk3(list, size) {
   return resultArray;
 }
 
-console.log(chunk3([1, 2, 3, 4, 5], 2));
-console.log(chunk3([1, 2, 3, 4, 5, 6], 2));
-console.log(chunk3([1, 2, 3, 4, 5, 6, 7], 4));
+function chunk5(list, size) {
+  let array = []; //临时数组
+  let resultArray = []; //结果数组
+  list.forEach((item, index) => {
+    array.push(item); //把item先放入临时数组
+    if (array.length == size) {
+      //当临时数组里的元素有size个时
+      resultArray.push(array); //把临时数组整个放入结果数组
+      array = []; //清空临时数组
+    }
+  });
+  if (array.length !== 0) {
+    //若临时数组里有剩余元素（当有余数时）
+    resultArray.push(array);
+  }
+  return resultArray;
+}
+
+console.log(chunk5([1, 2, 3, 4, 5], 2));
+console.log(chunk5([1, 2, 3, 4, 5, 6], 2));
+console.log(chunk5([1, 2, 3, 4, 5, 6, 7], 4));
