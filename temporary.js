c07e18cc7fdaa82ac7fcc8a0d5c1897d5aec0316
222 <zchaos2@foxmail.com>
2022-10-23 12:09:02 +0800

    找到一个优雅的写法

--- temporary.js
+++ temporary.js
@@ -1,23 +1,34 @@
-function temporary(nums,target){
-    let array=[];
-    let tempNum;
-    if(target<0){
-        nums=[];
+
+function temporary(nums, target) {
+    let hash = new Map();
+    let array = [];
+    if (target < 0) {
+        nums = [];
     }
-    
-    for(let i=0;i<nums.length;i++){
-        tempNum=target-nums[i];
-        if( nums.indexOf(tempNum)!==0){
-        array.push(i);
-        array.push(nums.indexOf(tempNum));
-        break;
+    for (let i = 0; i < nums.length; i++) {
+        if(hash.has(target-nums[i])){
+            return [hash.get(target -nums[i]),i];
+        }else{
+            hash.set(nums[i],i)
+            console.log(hash)
         }
     }
-    return array;
 }
+    /* for(let i=0;i<nums.length;i++){
+         tempNum=target-nums[i];
+         if(nums.indexOf(tempNum)!==0){
+         array.push(i);
+         array.push(nums.indexOf(tempNum));
+         break;
+         }
+     }
+     return array;
+ }
+ 
+ console.log(temporary([2,7,11,15],9))
+ */
 
-console.log(temporary([2,7,11,15],9))
-
-
+   
+console.log(temporary([2, 7, 11, 15], 9))
 module.exports = temporary;
 
