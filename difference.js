0c499544abffd45acf2ae6f67b61a502d6de37d9
222 <zchaos2@foxmail.com>
2022-10-25 22:03:09 +0800

    完善了difference

--- difference.js
+++ difference.js
@@ -1,17 +1,15 @@
 function difference(array, example) {
   let hash = new Map();
   let result = [];
-  array.forEach((item) => {
+  example.forEach((item) => {
     hash.set(item);
   });
   array.forEach((item, index) => {
-    if (!hash.has(example[index])) {
+    if (!hash.has(item)) {
       result.push(item);
     }
   });
   return result;
 }
 module.exports = difference;
-console.log(difference([3, 2, 1], [4, 0]));
-console.log(difference([3, 2, 1], [4, 2, 1]));
-console.log(difference([3, 2, 1], [2, 3]));
+console.log(difference([3, 2, 1], [4, 2, 0]));
--- difference.test.js
+++ difference.test.js
@@ -1,9 +1,12 @@
 const difference = require('./difference');
 describe('测试 difference 函数', () => {
   it('基本使用', () => {
-    expect(difference([3, 2, 1], [4, 2])).toEqual([3, 1]);
+    expect(difference([3, 2, 1], [4, 2, 0])).toEqual([3, 1]);
   });
-  it('负数使用', () => {
-    expect(difference([3, -2, 1], [4, 2])).toEqual([3, -2, 1]);
+  it('后比前长时', () => {
+    expect(difference([3], [4, 2])).toEqual([3]);
+  });
+  it('前比后长时', () => {
+    expect(difference([3, 2, 1], [2])).toEqual([3, 1]);
   });
 });
