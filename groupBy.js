52db6793a89ddd8cfecf1d1d2b6626fe2a65fee4
chenshuai2144 <qixian.cs@outlook.com>
2022-10-28 21:45:35 +0800

    add docs

--- groupBy.js
+++ groupBy.js
@@ -1,3 +1,9 @@
+/**
+ * 将数组的值按照回调方法来进行分组
+ * @param  {} collection 要处理的数组
+ * @param  {} hs 回调方法
+ * @return object 分组后的数组
+ */
 function groupBy(collection, hs) {
   let result = {};
   collection.forEach((item) => {
